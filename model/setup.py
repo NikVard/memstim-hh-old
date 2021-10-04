977057f400bc1a3953fdbf48af75bf25d3b4ea01
NikVard <45063685+NikVard@users.noreply.github.com>
2021-10-04 11:25:09 +0200

    Setup w/ exceptions

--- model/setup.py
+++ model/setup.py
@@ -1,3 +1,4 @@
+import sys
 from brian2 import *
 from model.globals import *
 from model.HH_equations import *
@@ -6,40 +7,46 @@ def create_group(parameters, name):
     """ Universal function that creates a group of neurons according to input parameters """
     pop_size = parameters['N']
 
-    if parameters['type'] == 'PyCAN':
-        eqs = py_CAN_eqs
+    try:
+        if parameters['type'].lower() == 'pycan':
+            eqs = py_CAN_eqs
 
-        G = NeuronGroup(N=pop_size,
-            model=py_CAN_eqs,
-            threshold='v>V_th',
-            reset=reset_eqs,
-            refractory=refractory_time,
-            namespace={'size':cell_size_py, 'glu':1},
-            method=integ_method,
-            name=name)
+            G = NeuronGroup(N=pop_size,
+                model=py_CAN_eqs,
+                threshold='v>V_th',
+                reset=reset_eqs,
+                refractory=refractory_time,
+                namespace={'size':cell_size_py, 'glu':1},
+                method=integ_method,
+                name=name)
 
-    elif parameters['type'] == 'Py':
-        eqs = py_eqs
+        elif parameters['type'].lower() == 'py':
+            eqs = py_eqs
 
-        G = NeuronGroup(N=pop_size,
-            model=py_eqs,
-            threshold='v>V_th',
-            reset=reset_eqs,
-            refractory=refractory_time,
-            namespace={'size' : cell_size_py},
-            method=integ_method,
-            name=name)
+            G = NeuronGroup(N=pop_size,
+                model=py_eqs,
+                threshold='v>V_th',
+                reset=reset_eqs,
+                refractory=refractory_time,
+                namespace={'size' : cell_size_py},
+                method=integ_method,
+                name=name)
 
-    else:
-        eqs = inh_eqs
+        elif parameters['type'].lower() == 'inh':
+            eqs = inh_eqs
 
-        G = NeuronGroup(N=pop_size,
-            model=inh_eqs,
-            threshold='v>V_th',
-            refractory=refractory_time,
-            namespace={'size' : cell_size_inh},
-            method=integ_method,
-            name=name)
+            G = NeuronGroup(N=pop_size,
+                model=inh_eqs,
+                threshold='v>V_th',
+                refractory=refractory_time,
+                namespace={'size' : cell_size_inh},
+                method=integ_method,
+                name=name)
+        else:
+            raise RuntimeError('Cannot create group ' + name + ': Incorrect neuron type [' + parameters['type']']')
+    except e:
+        sys.stderr.write('Exception: ' + str(e))
+        sys.exit(1)
 
     # random initialization of neuron membrane potential
     G.v = '-60*mvolt-rand()*10*mvolt' # str -> individual init. val. per neuron
