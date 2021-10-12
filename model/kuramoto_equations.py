8067caeb63ed96ec2eb8e7eb21b1c4874c1fcea8
NikVard <45063685+NikVard@users.noreply.github.com>
2021-10-12 15:24:30 +0200

    Kuramoto equations, working, no reset

---
+++ model/kuramoto_equations.py
@@ -0,0 +1,42 @@
+"""
+--------------------------------------------------------------------------------
+Date: 11/10/2021
+
+@author: Nikolaos Vardalakis
+--------------------------------------------------------------------------------
+
+Implementation Notes
+--------------------------------------------------------------------------------
+    | 1: For more information, refer to: https://michaelsmclayton.github.io/travellingWaves.html
+    | 2:
+"""
+
+kuramoto_eqs = '''
+    dTheta/dt = ((omega + ((kN) * PIF)) * second**-1) : 1
+    PIF = .5 * (sin(ThetaPreInput - Theta)) : 1
+    Vm = sin(Theta)*mV : volt
+    ThetaPreInput : 1
+    omega : 1
+    kN : 1
+'''
+
+
+''' Keep for later
+# Define Gaussian function
+def gaussian(distance, sig):
+    return np.exp(-np.power(distance, 2.) / (2 * np.power(sig, 2.)))
+
+# Get Euclidean distance
+def euclideanDistance(postSynapticLocation, preSynapticLocation):
+    return np.linalg.norm(postSynapticLocation - preSynapticLocation)
+
+# Define function to get weight between two neurons
+locations = [[x,y] for x in range(groupLength) for y in range(groupLength)]
+@implementation('numpy', discard_units=True)
+@check_units(i=1, j=1, sig=1, result=1)
+def getDistance(i, j, sig=3):
+    preSynapticLocation = np.array(locations[int(i)])
+    postSynapticLocation = np.array(locations[int(j)])
+    distance = euclideanDistance(postSynapticLocation, preSynapticLocation)
+    return gaussian(distance, sig)
+'''
