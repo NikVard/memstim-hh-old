8748d48f479384a8312cea780c30636bef89e7e4
NikVard <45063685+NikVard@users.noreply.github.com>
2021-12-27 13:48:55 +0100

    Fixed multirun.sh script

--- multirun.sh
+++ multirun.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-RESDIR="./results"
+### Get the current date and start making the results directory
+CURRDATE=$(date +"%FT%H%M")
+RESBASE="./results"
+RESDIR="$RESBASE"_"$CURRDATE"
 
+### Check and make directory for saving results
 if [ -d $RESDIR ]; then
 	echo "Directory $RESDIR exists. Cleaning..."
 	rm $RESDIR/*.txt
@@ -13,18 +17,28 @@ fi
 echo "Creating txt output..."
 touch "$RESDIR/total.txt"
 
+### Make a subdirectory to store the processed used configuration files
+BACKCONF="$RESDIR/configs"
+if ! [ -d $BACKCONF ]; then
+	echo "Directory $BACKCONF does not exist. Creating..."
+	mkdir $BACKCONF
+fi
+
 
 CNT=0
 RESULT=""
 for FN_CONF in ./configs/*;
 	do
 		### Print current simulation start time [24H format] ###
-		CURRDATE=$(date +'%T')
+		CURRTIME=$(date +"%T")
 
-		echo "Running simulation $CNT with config file $FN_CONF at time $CURRDATE";
+		echo "Running simulation $CNT with config file $FN_CONF at time $CURRTIME";
 
-		python3 -c "from brian2 import *; clear_cache('cython');" > /dev/null 2>&1
-		time python3 run_sim_dumb.py -p $FN_CONF > "./results/sim_res_$CNT.txt" 2>&1 && echo Done
+        FNAME=$(basename $FN_CONF)
+        FNAME=$(echo $FNAME | cut -f 1 -d '.')
+
+        python3 -c "from brian2 import *; clear_cache('cython');" > /dev/null 2>&1
+		time python3 run_sim_dumb.py -p $FN_CONF > "./results/sim_${CNT}_${FNAME}.txt" 2>&1 && echo 'Done'
 		#python3 -run_sim_dumb.py > /dev/null 2>&1
 
 		if [ "$?" = 0 ]; then
@@ -35,4 +49,7 @@ for FN_CONF in ./configs/*;
 
 		echo "$CNT : $FN_CONF : $RESULT" >> "./results/total.txt"
 		let "CNT+=1"
+		
+		### Copy the config file used to results dir
+		cp $FN_CONF $BACKCONF
 	done
