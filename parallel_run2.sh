188bbdc182c6b893f250f38e00b32d2775f22f25
NikVard <45063685+NikVard@users.noreply.github.com>
2022-04-22 12:04:07 +0200

    Added results of input @ CA1

--- parallel_run2.sh
+++ parallel_run2.sh
@@ -60,7 +60,7 @@ for DIR in $CONF_DIRS;
                 ### Queue the simulation
                 let DELAY="CNT*30"
         		echo "Delayed: $DELAY seconds"
-                # sbatch --job-name="SIM_${FN_CONF}" --begin=now+$DELAY --time=60 --cpus-per-task=1 --mem-per-cpu=16G --ntasks=1 --export=FCONF=$FN_CONF,RESDIR=$RESDIR,CNT=$CNT,OUTDIR=$RESBASE run_sim.sh
+                sbatch --job-name="SIM_${FN_CONF}" --begin=now+$DELAY --time=60 --cpus-per-task=1 --mem-per-cpu=16G --ntasks=1 --export=FCONF=$FN_CONF,RESDIR=$RESDIR,CNT=$CNT,OUTDIR=$RESBASE run_sim.sh
 
                 let "CNT+=1"
 
